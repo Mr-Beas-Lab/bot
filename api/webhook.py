a622a0fb72d5f7114985bec55e5db4a8f3304308
million-art <millionmulugeta09@gmail.com>
2025-01-02 21:56:28 +0300

    push

---
+++ api/webhook.py
@@ -0,0 +1,145 @@
+from http.server import BaseHTTPRequestHandler
+import os
+import json
+import asyncio
+import requests
+import datetime
+from telebot.async_telebot import AsyncTeleBot  
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
+from telebot import types
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from dotenv import load_dotenv
+
+
+load_dotenv()
+# Initialize bot
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+print(BOT_TOKEN)
+bot = AsyncTeleBot(BOT_TOKEN)
+
+# Initializee Firebase
+
+firebase_config = json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT'))
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred, {'storageBucket': "mrjohn-8ee8b.appspot.com"})
+db = firestore.client()
+bucket = storage.bucket()
+
+
+def generate_start_keyboard():
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(InlineKeyboardButton("Open Web App", web_app=WebAppInfo(url="https://mrb-crypto.vercel.app")))
+    return keyboard
+
+
+@bot.message_handler(commands=['start'])  
+async def start(message):
+    user_id = str(message.from_user.id)  
+    user_first_name = str(message.from_user.first_name)  
+    user_last_name = message.from_user.last_name
+    user_username = message.from_user.username
+    user_language_code = str(message.from_user.language_code)
+    is_premium = message.from_user.is_premium
+    text = message.text.split()
+    welcome_message = (  
+        f"Hello {user_first_name} {user_last_name}! 👋\n\n"
+        f"Welcome to Mr. John.\n\n"
+        f"Here you can earn coins!\n\n"
+        f"Invite friends to earn more coins together, and level up faster! 🧨\n"
+    )
+
+    try:
+        user_ref = db.collection('users').document(user_id)
+        user_doc = user_ref.get()
+
+        if not user_doc.exists:
+        
+
+            # Prepare user data
+            user_data = {
+                'userImage': None,
+                'firstName': user_first_name,
+                'lastName': user_last_name,
+                'username': user_username,
+                'languageCode': user_language_code,
+                'isPremium': is_premium,
+                'balance': 0,
+                'daily': {
+                    'claimedTime': None,
+                    'claimedDay': 0
+                },
+                'WalletAddress': None,
+                'exchangeKey': {
+                    'apiKey': None,
+                    'secretKey': None,
+                },
+                'buy_analyzer_tool': {
+                    'isActive': False,
+                    'duration': None,  
+                    'amount': 0,      
+                    'expirationDate': None,
+                    'lastPurchase': firestore.SERVER_TIMESTAMP  # Automatically set to the current time
+                }
+            }
+
+            if len(text) > 1 and text[1].startswith('ref_'):   
+                referrer_id = text[1][4:]
+                referrer_ref = db.collection('users').document(referrer_id)
+                referrer_doc = referrer_ref.get()
+
+                if referrer_doc.exists:
+                    user_data['referredBy'] = referrer_id
+                    referrer_data = referrer_doc.to_dict()
+                    bonus_amount = 500 if is_premium else 100
+                    current_balance = referrer_data.get('balance', 0)
+                    new_balance = current_balance + bonus_amount
+
+                    referrals = referrer_data.get('referrals', {})
+                    if referrals is None:
+                        referrals = {}
+                    referrals[user_id] = {
+                        'addedValue': bonus_amount,
+                        'firstName': user_first_name,
+                        'lastName': user_last_name,
+                        'userImage': None,
+                    }  
+
+                    referrer_ref.update({
+                        'balance': new_balance,
+                        'referrals': referrals
+                    })
+                else:
+                    user_data['referredBy'] = None
+
+            user_ref.set(user_data)
+
+        keyboard = generate_start_keyboard()
+        await bot.reply_to(message, welcome_message, reply_markup=keyboard)  
+    except Exception as e:
+        error_message = "Error. Please try again!"
+        await bot.reply_to(message, error_message)  
+        print(f"Error occurred: {str(e)}")  
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])  
+        post_data = self.rfile.read(content_length)
+        update_dict = json.loads(post_data.decode('utf-8'))
+
+        asyncio.run(self.process_update(update_dict))
+
+        self.send_response(200)
+        self.end_headers()
+
+    async def process_update(self, update_dict):
+        update = types.Update.de_json(update_dict)
+        await bot.process_new_updates([update])
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write('Hello, BOT is running!'.encode('utf-8'))
+
+
+ 
