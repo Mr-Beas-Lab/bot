39bbb823f27ecf34f1e5f2f9a8f5ced8c6c44a55
million-art <millionmulugeta09@gmail.com>
2025-01-23 18:19:13 +0300

    language update

---
+++ api/message.py
@@ -0,0 +1,37 @@
+# messages.py
+
+def get_welcome_messages(first_name):
+    return {
+        'english': f"Hello {first_name}! 👋\n\n"
+                   "Welcome to Mr. Beas.\n"
+                   "Your ultimate trading companion, inspired by the legendary trader Elias Arraez and supported by the thriving Grupo Arraez community of 400+ traders.\n\n"
+                   "Mr. Beas is revolutionizing cryptocurrency trading by:\n\n"
+                   "- **AI-Driven Insights**: An intelligent agent trained with the proven strategies and techniques of Elias Arraez, ensuring smarter and more profitable trades.\n"
+                   "- **Accurate Market Analysis**: Powered by a proven algorithm that identifies optimal entry and exit points in the market.\n"
+                   "- **Enhanced Risk Management**: Minimize losses and maximize profits with actionable insights tailored to market conditions.\n\n"
+                   "Community Token: The $MRB token is backed by a dedicated community of traders from the Grupo Arraez.\n\n"
+                   "Powered by the $MRB token on the TON blockchain, Mr. Beas transforms how traders analyze markets and manage risks.\n\n"
+                   "Join the fastest-growing trading ecosystem today and take your trading to the next level!",
+
+        'spanish': f"¡Hola {first_name}! 👋\n\n"
+                   "Bienvenido a Mr. Beas.\n"
+                   "Tu compañero de trading definitivo, inspirado por el legendario trader Elias Arraez y respaldado por la próspera comunidad de Grupo Arraez de más de 400 traders.\n\n"
+                   "Mr. Beas está revolucionando el trading de criptomonedas:\n\n"
+                   "- **Información impulsada por IA**: Un agente inteligente entrenado con las estrategias y técnicas probadas de Elias Arraez, asegurando operaciones más inteligentes y rentables.\n"
+                   "- **Análisis de mercado preciso**: Impulsado por un algoritmo probado que identifica los puntos de entrada y salida óptimos en el mercado.\n"
+                   "- **Gestión de riesgos mejorada**: Minimiza pérdidas y maximiza ganancias con información práctica adaptada a las condiciones del mercado.\n\n"
+                   "Token comunitario: El token $MRB cuenta con el respaldo de una comunidad dedicada de traders del Grupo Arraez.\n\n"
+                   "Impulsado por el token $MRB en la blockchain TON, Mr. Beas transforma la forma en que los traders analizan los mercados y gestionan riesgos.\n\n"
+                   "¡Únete al ecosistema de trading de más rápido crecimiento hoy y lleva tu trading al siguiente nivel!",
+
+        'chinese': f"你好 {first_name}！👋\n\n"
+                   "欢迎来到 Mr. Beas。\n"
+                   "您的终极交易伴侣，灵感来自传奇交易员 Elias Arraez，并得到 400 多名交易员的 Grupo Arraez 社区的支持。\n\n"
+                   "Mr. Beas 正在革新加密货币交易：\n\n"
+                   "- **人工智能驱动的洞察**：一个智能代理，经过 Elias Arraez 的成功策略和技巧训练，确保更智能和更有利可图的交易。\n"
+                   "- **准确的市场分析**：由一个经过验证的算法提供支持，识别市场中的最佳进出点。\n"
+                   "- **增强的风险管理**：通过量身定制的可操作性洞察，最大限度地减少损失并最大化利润。\n\n"
+                   "社区代币：$MRB 代币由 Grupo Arraez 的交易员社区支持。\n\n"
+                   "在 TON 区块链上由 $MRB 代币提供支持，Mr. Beas 转变了交易员分析市场和管理风险的方式。\n\n"
+                   "今天加入增长最快的交易生态系统，让您的交易提升到一个新水平！"
+    }
--- api/webhook.py
+++ api/webhook.py
@@ -1,30 +1,31 @@
-from http.server import BaseHTTPRequestHandler
 import os
 import json
-import asyncio
 import requests
+import asyncio
+import datetime
 from telebot.async_telebot import AsyncTeleBot
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 from telebot import types
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from dotenv import load_dotenv
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from message import get_welcome_messages
 
+# Load environment variables
 load_dotenv()
 
-# Initialize bot
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+# Initialize Telegram Bot
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = AsyncTeleBot(BOT_TOKEN)
 
 # Initialize Firebase
-firebase_config = json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT'))
+firebase_config = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
 cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred, {'storageBucket': "mrjohn-8ee8b.appspot.com"})
 db = firestore.client()
 bucket = storage.bucket()
 
- 
-# Function to generate the main keyboard with language selection and additional buttons
+# Function to generate main keyboard with language selection
 def generate_main_keyboard(selected_language=None):
     keyboard = types.InlineKeyboardMarkup(row_width=3)
     languages = {
@@ -36,20 +37,16 @@ def generate_main_keyboard(selected_language=None):
     buttons = []
     for callback_data, label in languages.items():
         if selected_language and callback_data.endswith(selected_language):
-            label += " ✅"  # Add the checkmark for the selected language
+            label += " ✅"
         buttons.append(types.InlineKeyboardButton(label, callback_data=callback_data))
 
-    # Add the language buttons in rows
-    keyboard.add(*buttons)
-
-    # Add additional buttons in separate rows
     keyboard.add(
         types.InlineKeyboardButton("📢 Join Channel", url="https://t.me/mrbeas_group"),
         types.InlineKeyboardButton("🚀 Launch App", web_app=types.WebAppInfo(url="https://mrb-theta.vercel.app"))
     )
     return keyboard
 
-# Handle '/start' command
+# Handle /start command
 @bot.message_handler(commands=['start'])
 async def start(message):
     user_id = str(message.from_user.id)
@@ -58,6 +55,25 @@ async def start(message):
         user_doc = user_ref.get()
 
         if not user_doc.exists:
+            # Handle profile photo upload
+            photo = await bot.get_user_profile_photos(user_id, limit=1)
+            user_image = None
+            if photo.total_count > 0:
+                file_id = photo.photos[0][-1].file_id
+                file_info = await bot.get_file(file_id)
+                file_path = file_info.file_path
+                file_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_path}"
+
+                # Download the image
+                response = requests.get(file_url)
+                if response.status_code == 200:
+                    # Upload the image to Firebase storage
+                    blob = bucket.blob(f"user_{user_id}.jpg")
+                    blob.upload_from_string(response.content, content_type="image/jpeg")
+
+                    # Generate signed URL for the image
+                    user_image = blob.generate_signed_url(datetime.timedelta(days=365), method="GET")
+
             # Initialize user data without language selected yet
             user_data = {
                 'firstName': message.from_user.first_name,
@@ -65,13 +81,22 @@ async def start(message):
                 'username': message.from_user.username,
                 'languageCode': None,
                 'isPremium': message.from_user.is_premium,
+                'referrals': {},
                 'balance': 0,
                 'daily': {'claimedTime': None, 'claimedDay': 0},
-                'WalletAddress': None
+                'WalletAddress': None,
+                'userImage': user_image
             }
-            text = message.text.split()  
-            if len(text) > 1 and text[1].startswith('ref_'):   
+
+            text = message.text.split()
+
+            # Debugging to see the referral part
+            print(f"Referral text: {text}")
+
+            if len(text) > 1 and text[1].startswith('ref_'):
                 referrer_id = text[1][4:]
+                print(f"Referrer ID: {referrer_id}")
+
                 referrer_ref = db.collection('users').document(referrer_id)
                 referrer_doc = referrer_ref.get()
 
@@ -87,26 +112,28 @@ async def start(message):
                         'addedValue': bonus_amount,
                         'firstName': message.from_user.first_name,
                         'lastName': message.from_user.last_name,
-                        'userImage': message.from_user.photo_url,
+                        'userImage': user_image
                     }
 
                     referrer_ref.update({
                         'balance': new_balance,
                         'referrals': referrals
                     })
+                    
+                    print(f"Updated referrer balance: {new_balance} and referrals: {referrals}")
                 else:
+                    print(f"Referrer document does not exist for ID: {referrer_id}")
                     user_data['referredBy'] = None
 
             user_ref.set(user_data)
 
-        # Define welcome message and language selection
         welcome_message = f"Hello {message.from_user.first_name}! 👋\n\nWelcome to Mr. John.\nHere you can earn coins!\nInvite friends to earn more coins together, and level up faster! 🧨"
         keyboard = generate_main_keyboard()
-        await bot.reply_to(message, welcome_message, reply_markup=keyboard)  
+        await bot.reply_to(message, welcome_message, reply_markup=keyboard)
 
     except Exception as e:
         error_message = "Error. Please try again!"
-        await bot.send_message(message.chat.id, error_message)  
+        await bot.send_message(message.chat.id, error_message)
         print(f"Error occurred: {str(e)}")
 
 # Handle language selection callback
@@ -115,28 +142,18 @@ async def language_selection(call):
     user_id = str(call.from_user.id)
     selected_language = call.data.split('_')[1]
 
-    # Save the selected language in the user's data
     user_ref = db.collection('users').document(user_id)
     user_ref.update({'languageCode': selected_language})
 
-    # Define welcome messages in different languages
-    messages = {
-        'english': f"Hello {call.from_user.first_name}! 👋\n\nWelcome to Mr. John.\nHere you can earn coins!\nInvite friends to earn more coins together, and level up faster! 🧨",
-        'chinese': f"你好 {call.from_user.first_name}！👋\n\n欢迎来到Mr. John。\n在这里你可以赚取硬币！\n邀请朋友一起赚取更多硬币，快速升级！🧨",
-        'spanish': f"¡Hola {call.from_user.first_name}! 👋\n\nBienvenido a Mr. John.\n¡Aquí puedes ganar monedas!\nInvita amigos para ganar más monedas juntos y subir de nivel más rápido! 🧨"
-    }
+    # Use the imported function to get the welcome messages
+    welcome_messages = get_welcome_messages(call.from_user.first_name)
 
-    # Send the welcome message based on selected language
-    welcome_message = messages.get(selected_language, messages['english'])
-
-    # Show main menu with the updated language selection
+    welcome_message = welcome_messages.get(selected_language, welcome_messages['english'])
     keyboard = generate_main_keyboard(selected_language)
     await bot.edit_message_text(welcome_message, chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=keyboard)
 
-
-
-# Handle incoming updates
-class handler(BaseHTTPRequestHandler):
+# HTTP Server to handle updates from Telegram Webhook
+class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])  
         post_data = self.rfile.read(content_length)
@@ -155,3 +172,4 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write('Hello, BOT is running!'.encode('utf-8'))
+ 
