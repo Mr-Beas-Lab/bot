7dd3db81fa26576b1ea7f96fc4215cfa0d1d6bd2
million <74927604+Million-art@users.noreply.github.com>
2025-01-25 11:56:24 +0300

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -83,6 +83,7 @@ async def start(message):
                 'isPremium': message.from_user.is_premium,
                 'referrals': {},
                 'balance': 0,
+                'tasks':[],
                 'daily': {'claimedTime': None, 'claimedDay': 0},
                 'WalletAddress': None,
                 'userImage': user_image
